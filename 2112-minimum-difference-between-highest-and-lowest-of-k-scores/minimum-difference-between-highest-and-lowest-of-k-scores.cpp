c65c604dba7294df578ce6e5fa6532f1848d79f5
Bhuma vignesh <vigneshvigge33@gmail.com>
2026-08-14 01:35:52 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (57.05%) - LeetSync

--- 2112-minimum-difference-between-highest-and-lowest-of-k-scores/minimum-difference-between-highest-and-lowest-of-k-scores.cpp
+++ 2112-minimum-difference-between-highest-and-lowest-of-k-scores/minimum-difference-between-highest-and-lowest-of-k-scores.cpp
@@ -3,16 +3,11 @@ public:
     int minimumDifference(vector<int>& nums, int k) {
         int n=nums.size();
         int mind=INT_MAX;
-        if(n==1){
-            return 0;
-        }
-        else{
         sort(nums.begin(),nums.end());
         for(int i=0;i<=n-k;i++){
             int diff=nums[k+i-1]-nums[i];
             mind=min(diff,mind);
         }
         return mind; 
-        }
     }
 };
