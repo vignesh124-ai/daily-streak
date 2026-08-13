0b5f9b8295db6fa09cc0178e56ec2e8eb7ac4e7f
Bhuma vignesh <vigneshvigge33@gmail.com>
2026-08-14 01:47:03 +0530

    Time: 445 ms (50.49%) | Memory: 0B (100.00%) - LeetSync

--- 175-combine-two-tables/combine-two-tables.sql
+++ 175-combine-two-tables/combine-two-tables.sql
@@ -1,1 +1,1 @@
-select firstName,lastName,city,state from Person left join Address on Person.personId=Address.personId;
+select Person.firstName,Person.lastName,Address.city,Address.state from Person left join Address on Person.personId=Address.personId;
