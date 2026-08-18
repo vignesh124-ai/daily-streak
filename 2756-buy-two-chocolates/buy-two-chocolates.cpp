a5da8e57d3b397b281d3731deccd40afc6d9409c
Bhuma vignesh <vigneshvigge33@gmail.com>
2026-08-19 01:29:21 +0530

    Time: 0 ms (100.00%) | Memory: 52.1 MB (95.64%) - LeetSync

---
+++ 2756-buy-two-chocolates/buy-two-chocolates.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int buyChoco(vector<int>& prices, int money) {
+        sort(prices.begin(),prices.end());
+        int min=prices[0]+prices[1];
+        if(min<=money){
+            return money-min;
+        }
+        else{
+            return money;
+        }
+    }
+};
