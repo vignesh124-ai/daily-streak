a0b028abed704a2cec1afdeacdbb80ba74480221
Bhuma vignesh <vigneshvigge33@gmail.com>
2026-08-21 01:39:20 +0530

    Time: 3 ms (55.55%) | Memory: 9.6 MB (73.01%) - LeetSync

--- 43-multiply-strings/multiply-strings.cpp
+++ 43-multiply-strings/multiply-strings.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     string multiply(string num1, string num2){
+        if (num1=="0" || num2=="0"){
+            return "0";}
         int l=num1.length()+num2.length();
         vector<int> arr(l,0);
         for(int i=num1.length()-1;i>=0;i--){
